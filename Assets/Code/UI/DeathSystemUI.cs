7fbdbd7b66a3f8be172c05fdf03b92064ef54a44
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2021-10-04 17:47:10 +0200

    Added Death Counter

---
+++ Assets/Code/UI/DeathSystemUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class DeathSystemUI : MonoBehaviour
+    {
+        public static Action ONDeathAddedEvent;
+        [SerializeField] private TMP_Text scoreText;
+        private int _currentDeaths;
+        
+        private void Start()
+        {
+            ONDeathAddedEvent += AddDeath;
+        }
+
+        private void AddDeath()
+        {
+            _currentDeaths += 1;
+            UpdateUIText();
+        }
+
+        private void UpdateUIText()
+        {
+            scoreText.SetText(_currentDeaths.ToString());
+        }
+    }
+}
